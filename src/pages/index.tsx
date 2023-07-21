0d895e0558c5799d01e0c502f3c0eb9b9c9b0934
Kavindi Peiris <kavindi.peiris@upraxis.com>
2023-07-22 03:28:59 +0530

    feat: added animation to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { motion } from "framer-motion";
 import {
   createStyles,
   Container,
@@ -6,7 +8,6 @@ import {
   Group,
   rem,
 } from "@mantine/core";
-import Link from "next/link";
 
 const useStyles = createStyles((theme) => ({
   wrapper: {
@@ -73,6 +74,38 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
+const pieEmojiAnimation = {
+  y: [0, -20, 0], // Bounce up and down by 20 pixels
+  transition: {
+    repeat: Infinity, // Repeat the animation infinitely
+    duration: 1, // Each animation cycle takes 1 second
+  },
+};
+
+export function BouncingPieEmoji() {
+  return (
+    <motion.span animate={pieEmojiAnimation} style={{ display: "inline-block" }}>
+      🥧
+    </motion.span>
+  );
+}
+
+export function ShakingMoneyEmoji() {
+  const moneyEmojiAnimation = {
+    rotate: [-10, 10, -10], // Rotate left and right
+    transition: {
+      repeat: Infinity, // Repeat the animation infinitely
+      duration: 1, // Each animation cycle takes 1 second
+    },
+  };
+
+  return (
+    <motion.span animate={moneyEmojiAnimation} style={{ display: "inline-block" }}>
+      🤑
+    </motion.span>
+  );
+}
+
 export function Landing() {
   const { classes } = useStyles();
 
@@ -80,32 +113,22 @@ export function Landing() {
     <div className={classes.wrapper}>
       <Container size={700} className={classes.inner}>
         <h1 className={classes.title}>
-          🤑{" "}
-          <Text
-            component="span"
-            variant="gradient"
-            inherit
-          >
+        <ShakingMoneyEmoji />{" "}
+          <Text component="span" variant="gradient" inherit>
             Crypto
           </Text>{" "}
           made
           <br />
-          as easy as 🥧 pie.
+          as easy as  <BouncingPieEmoji /> pie.
         </h1>
         <Text className={classes.description} color="dimmed">
           We simplify crypto for everyone, while keeping it safe and secure. Own
           your crypto the easy way.
         </Text>
         <Group className={classes.controls}>
-          <Link href="/onboard">
-          <Button
-            size="xl"
-            className={classes.control}
-            variant="gradient"
-          >
+          <Button size="xl" className={classes.control} variant="gradient">
             Get started
           </Button>
-          </Link>
         </Group>
       </Container>
     </div>
